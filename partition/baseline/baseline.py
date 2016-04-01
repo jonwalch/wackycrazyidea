76698d497aecb111f40ccc27f5d38f7cefb40a47
jonwalch <jonwalch@gmail.com>
2016-04-01 15:43:31 -0400

    more progress

--- partition/baseline/baseline.py
+++ partition/baseline/baseline.py
@@ -7,37 +7,44 @@ class Node:
     self.neighbors = neighbors
     self.groupNum = -1
 
-if len(sys.argv) != 4:
-  print("wrong num cmd line args. usage is python3.3 baseline.py file1 file2 num_partition")
-  return 1
-
 def baseline1():
+
+  if len(sys.argv) != 4:
+    print("wrong num cmd line args. usage is python3.3 baseline.py file1 file2 num_partition")
+    return 1
+
   edgeListFile = sys.argv[1]
   nodeNumNeigh = sys.argv[2]
 
   partition_num = sys.argv[3] 
   edge_num = sum(1 for line in open(edgeListFile))
+  threshhold = edge_num / partition_num
 
   nodes = [] #each node has .neighbor
 
   with open(nodeNumNeigh) as f:
     for line in f:
       split = line.split(":")
-      tempNodeNum = split[0]
+      curNodeNum = split[0]
       neighbors = []
       for i in range (1, len(split)):
         neighbors.append(split[i])
-      newnode = Node(tempNodeNum, neighbors)
+      newnode = Node(curNodeNum, neighbors)
+      nodes.append(newnode)
 
   curGroupEdges = 0
   group_num = 0
 
+  output = open(baseline1.out, 'w')
+
   for i,node in enumerate(nodes):
-    while curGroupEdges < edge_num / partition_num:
+    while curGroupEdges + len(node.neighbors) < threshold and i < len(nodes):
       nodes[i].groupNum = group_num
       curGroupEdges += len(node.neighbors)
+      output.write(str(i) + " " + group_num)
+      i += 1
     group_num += 1
-  curGroupEdges = 0
+    curGroupEdges = 0
 
 if __name__ == "__main__":
   baseline1()
