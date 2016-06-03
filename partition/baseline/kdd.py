6fbd5cd3415cb996b6f4e94dd024185df37eefdf
jonwalch <jonwalch@gmail.com>
2016-06-03 14:26:02 -0400

    finished random partitioning

--- partition/baseline/kdd.py
+++ partition/baseline/kdd.py
@@ -2,6 +2,7 @@
 import sys
 import collections
 import math
+import random
 
 def main():
 
@@ -27,55 +28,90 @@ def main():
         numVertex += 1
         #print("vertex " + str(vertex) + " is in parition " + str(partition))
 
+  edgesDone = 0
+  edgesToCut = 0
+
   with open(edgeList) as f:
     for line in f:
       if line[0] != "#":
         split = line.split()
         u = int(split[0])
         v = int(split[1])
-
         partitionNum = vertexCluster[u]
+
         if vertexCluster[u] == vertexCluster[v] and (v,u) not in edgeCluster[partitionNum]:
           edgeCluster[partitionNum].append((u,v))
+          edgesDone += 1
 
         elif (u,v) not in cutEdges[partitionNum] and (v,u) not in cutEdges[partitionNum]:
           cutEdges[vertexCluster[u]].append((u,v))
           cutEdges[vertexCluster[v]].append((u,v))
+          edgesToCut += 1
+
+        #else: #duplicate or v,u already accounted for
+          #print((u,v))
+
+  print(edgesDone)
+  print(edgesToCut)
+  numEdges = edgesDone + edgesToCut
 
   pairList = []
-  
-  for i in range(numVertex - 1):
-    for j in range(i + 1, numVertex - 1):
+
+  for i in range(len(cutEdges)):
+    for j in range(i + 1, len(cutEdges)):
       pairList.append((i,j))
 
   for i,j in pairList:
     setA = []
     setB = []
 
-    if len(cutEdges[i]) == 0 or len(cutEdges[j]) == 0:
+    sharedEdges = set(cutEdges[i]).intersection(set(cutEdges[j])) #find common elements between the two
+    sharedEdges = list(sharedEdges)
+    #print(sharedEdges)
+
+    if len(sharedEdges) == 0: #if no edges to partition go to next iteration
       continue
 
-    if len(cutEdges[i]) % 2 == 0:
-      sizeSetA = len(cutEdges[i]) / 2
+    if len(sharedEdges)% 2 == 0:
+      sizeSetA = int(len(sharedEdges) / 2)
       sizeSetB = sizeSetA
     else:
-      sizeSetA = floor(len(cutEdges[i]) / 2)
+      sizeSetA = int(math.floor(len(sharedEdges) / 2))
       sizeSetB = sizeSetA + 1
 
-    #for each edge between clusters
-      if len(setA) == setSizeA:
-        setB.append(u,v)
-      elif len(setB) == setsizeB:
-        setA.append(u,v)
+    for (u,v) in sharedEdges:
+    #after each append here, must remove edge from opposite set's cutEdges
+      if len(setA) == sizeSetA:
+        setB.append((u,v))
+        cutEdges[i].remove((u,v))
+        cutEdges[j].remove((u,v))
+      elif len(setB) == sizeSetB:
+        setA.append((u,v))
+        cutEdges[i].remove((u,v))
+        cutEdges[j].remove((u,v))
       else:
         rand = random.choice([True, False])
         if rand:
-          setA.append(u,v)
+          setA.append((u,v))
+          cutEdges[i].remove((u,v))
+          cutEdges[j].remove((u,v))
         else:
-          setB.append(u,v)
+          setB.append((u,v))
+          cutEdges[i].remove((u,v))
+          cutEdges[j].remove((u,v))
 
-    edgeCluster[vertexCluster[u]].append(setA)
-    edgeCluster[vertexCluster[v]].append(setB)
+    edgeCluster[i] += setA
+    edgeCluster[j] += setB
+
+  partitionedEdges = 0
+
+  for i in edgeCluster:
+    print(i)
+    print(edgeCluster[i])
+    partitionedEdges += len(edgeCluster[i])
+
+  print(numEdges)
+  print(partitionedEdges)
 
 if __name__ == "__main__":
   main()
